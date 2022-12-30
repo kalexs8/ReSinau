2d34c6febd058fc24d8b63fd3b8b14ac5c169a68
ultimatelite <89692490+ultimatelite@users.noreply.github.com>
2022-12-30 16:37:47 +0700

    Revised Function to automate Kunci Jawaban

--- data_generator.py
+++ data_generator.py
@@ -1,17 +1,38 @@
 import re
+from sys import argv
+
+if len(argv) < 2:
+    raise Exception("Length argumen minimal harus 2")
 
 soalPattern = re.compile(r"^\d+\..*(\.{3}|=|\?|\.{3} |= |\? )$", re.IGNORECASE | re.MULTILINE)
 jawabPattern = re.compile(r"^[a-d|A-D]*\.", re.IGNORECASE)
 
+base_source = ""
+out_soal = ""
+out_jawab = ""
+out_kunci = ""
+
+with open(argv[1]) as f:
+    x = f.read().split("\n")
+    if len(x) < 4 or len(x) > 4:
+        raise Exception("Length file {} harus 4".format(argv[1]))
+    base_source = x[0].split("=")[1]
+    out_soal = x[1].split("=")[1]
+    out_jawab = x[2].split("=")[1]
+    out_kunci = x[3].split("=")[1]
+
 soal = []
 jawab = []
+kunci_jawab = []
 
-with open("data.txt", "r") as f:
+with open(base_source, "r") as f:
     s = f.read()
     pecahan = s.split("\n")
     st = ""
     for idx in range(0, len(pecahan)):
         i = pecahan[idx]
+        if i.lower() == "--kunci":
+            break
         if len(i) < 1:
             continue
 
@@ -28,6 +49,24 @@ with open("data.txt", "r") as f:
             if len(st) > 0:
                 soal.append(st)
                 st = ""
+    encountered_kunci = False
+    kunci_jawaban = []
+    for i in pecahan:
+        if i.lower() == "--kunci":
+            encountered_kunci = True
+        
+        if encountered_kunci and i.lower() != "--kunci":
+            should = 0
+            if i.lower() == "b":
+                should = 1
+            elif i.lower() == "c":
+                should = 2
+            elif i.lower() == "d":
+                should = 3
+            kunci_jawaban.append(should)
+            if len(kunci_jawaban) == 3:
+                kunci_jawab.append(kunci_jawaban)
+                kunci_jawaban = []
 
 
 soal_len = len(soal)
@@ -55,7 +94,24 @@ for i in range(0, 2):
 del temp
 del jtmp
 
-with open("soal.txt", "w") as f:
+
+len_kunci = len(kunci_jawab)
+len_jawaban = len(jawab)
+
+if len_kunci != len_jawaban:
+    if len_kunci > len_jawaban:
+        raise Exception("Kunci jawaban melebihi jawaban segmen, harusnya terdapat {} kunci segmen, tetapi lebih {}."
+        .format(len_jawaban, len_kunci - len_jawaban)
+        + " (Segmen Kunci Jawaban: {}, Segmen Jawaban: {})".format(len_kunci, len_jawaban))
+    else:
+        raise Exception("Kunci jawaban kurang dari jawaban segmen, harusnya terdapat {} kunci segmen, tetapi kurang {}."
+        .format(len_jawaban, len_jawaban - len_kunci)
+        + " (Segmen Kunci Jawaban: {}, Segmen Jawaban: {})".format(len_kunci, len_jawaban))
+
+del len_kunci
+del len_jawaban
+
+with open(out_soal, "w") as f:
     counter = 1
     f.write("listOf(\n")
     for i in soal:
@@ -69,7 +125,7 @@ with open("soal.txt", "w") as f:
 
         counter += 1
 
-with open("jawaban.txt", "w") as f:
+with open(out_jawab, "w") as f:
     len_jwb = len(jawab)
     f.write("listOf(\n")
     for i in range(0, len_jwb):
@@ -88,3 +144,17 @@ with open("jawaban.txt", "w") as f:
             f.write(")")
         else:
             f.write("),\nlistOf(\n")
+
+with open(out_kunci, "w") as f:
+    f.write("listOf(")
+    jwb_k = len(kunci_jawab)
+    for i in range(0, jwb_k):
+        for j in range(0, len(kunci_jawab[i])):
+            if j == len(kunci_jawab[i])-1:
+                f.write("{}".format(kunci_jawab[i][j]))
+            else:
+                f.write("{},".format(kunci_jawab[i][j]))
+        if i == jwb_k-1:
+            f.write(")\n")
+        else:
+            f.write("),\nlistOf(")
